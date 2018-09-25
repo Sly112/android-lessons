4bd7533c1ceb347f2a7232263b27887dc4fa3fec
szielinski <s4zielinski@gmail.com>
2018-09-25 21:34:47 +0200

    review commit

--- app/src/main/java/com/empik/androidlessons/MainActivity.kt
+++ app/src/main/java/com/empik/androidlessons/MainActivity.kt
@@ -2,11 +2,14 @@ package com.empik.androidlessons
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("aa", "some log")
     }
 }
